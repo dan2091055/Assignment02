ae9b237a672ec0364042fea14170f62c77316914
daniel <daniel@172.31.98.218>
2019-06-18 18:16:26 -0700

    this is going to be the final version of the calculator.

--- Assignment_02/src/cse360assign2/AddingMachine.java
+++ Assignment_02/src/cse360assign2/AddingMachine.java
@@ -1,33 +1,44 @@
 /**Name: Daniel Martinez
  *Assignment #: 2
  *Description: this assignment is a calculator
+ *
  */
 package cse360assign2;
 
 public class AddingMachine {
 
-	private int total;
+	
+	private int total;//this variable is going to be storing the the total value of the calculator.
 	
 	
-	
-	String CalcHistory = "0";//this is going to be 
+	private String CalcHistory = "0";//this is the string where the calculation history is going to be stored.
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
+	/**
+	 * this function is just going to be returning the total value.
+	 * @return
+	 */
 	
 	public int getTotal () {//this is going to be getting the total
 		return total;
 	}
-	
+	/**
+	 * this function is going to be to adding to the total.
+	 * @param value
+	 */
 	public void add (int value) {//this is the add function
 		CalcHistory += " + " + Integer.toString(value);
 		
 		total = total += value;
 		
 		
-		
 	}
+	/**
+	 * this function is going to be subtracting from the total value.
+	 * @param value
+	 */
 	
 	public void subtract (int value) {//this is the subtract function
 		
@@ -36,12 +47,18 @@ public class AddingMachine {
 		total = total-= value;
 	
 	}
+	/**
+	 * this function is going to be returning the total.
+	 */
 		
 	public String toString () {
 	
 		return CalcHistory;
 	}
 
+	/**
+	 * this function is going to be clearing the total and the history
+	 */
 	public void clear() {
 		
 		CalcHistory = "";
