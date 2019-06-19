06f13dae2399f93cefc6a6e6b7e8a6daeb358abd
daniel <daniel@172.31.98.218>
2019-06-18 18:23:50 -0700

    this is the initial version

--- Assignment_02/src/cse360assign2/AddingMachine.java
+++ Assignment_02/src/cse360assign2/AddingMachine.java
@@ -3,66 +3,34 @@
  *Description: this assignment is a calculator
  *
  */
+
 package cse360assign2;
 
 public class AddingMachine {
 
-	
-	private int total;//this variable is going to be storing the the total value of the calculator.
-	
-	
-	private String CalcHistory = "0";//this is the string where the calculation history is going to be stored.
+	private int total;
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
-	/**
-	 * this function is just going to be returning the total value.
-	 * @return
-	 */
 	
-	public int getTotal () {//this is going to be getting the total
-		return total;
+	public int getTotal () {
+		return 0;
 	}
-	/**
-	 * this function is going to be to adding to the total.
-	 * @param value
-	 */
-	public void add (int value) {//this is the add function
-		CalcHistory += " + " + Integer.toString(value);
-		
-		total = total += value;
-		
+	
+	public void add (int value) {
 		
 	}
-	/**
-	 * this function is going to be subtracting from the total value.
-	 * @param value
-	 */
 	
-	public void subtract (int value) {//this is the subtract function
+	public void subtract (int value) {
 		
-		CalcHistory += " - " + Integer.toString(value);
-		
-		total = total-= value;
-	
 	}
-	/**
-	 * this function is going to be returning the total.
-	 */
 		
 	public String toString () {
-	
-		return CalcHistory;
+		return "";
 	}
 
-	/**
-	 * this function is going to be clearing the total and the history
-	 */
 	public void clear() {
-		
-		CalcHistory = "";
-		total = 0;
 	
 	}
 }
